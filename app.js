3a2b3963e0b158950bc2a52b47e80ae6a2559e88
mirkiy <mirizachar@gmail.com>
2023-04-04 19:55:58 +0100

    check winner logic

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const message = document.querySelector(".message");
 const buttons = document.querySelectorAll("button");
-console.log(buttons);
 
 const coinArray = ["Heads", "Tails"];
+let score = [0, 0];
 
 function tossCoin(e) {
-  console.log("player:" + e.target.innerText);
-  let computerToss = Math.floor(Math.random() * 2);
-  console.log("computer:" + coinArray[computerToss]);
+  let playerGuess = e.target.innerText;
+  let computerToss = Math.floor(Math.random() * coinArray.length);
+  let computerGuess = coinArray[computerToss];
+  message.innerHTML = "Computer Selected " + computerGuess + "<br>";
+  let output;
+  if (playerGuess === computerGuess) {
+    output = "Player Wins ";
+    score[0]++;
+  } else {
+    output = "Computer Wins ";
+    score[1]++;
+  }
+  message.innerHTML +=
+    output + "<br> Player " + score[0] + "Computer " + score[1];
 }
 
 for (let i = 0; i < buttons.length; i++) {
-  console.log(buttons[i]);
   buttons[i].addEventListener("click", tossCoin);
 }
