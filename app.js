c5d4128386f354413a40cd031a278cb24b8e4192
mirkiy <mirizachar@gmail.com>
2023-04-04 19:26:38 +0100

    function to handle toss

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const message = document.querySelector(".message");
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll("button");
 console.log(buttons);
 
+function tossCoin(e) {
+    console.log(e.target.innerText)
+}
+
+for (let i = 0; i < buttons.length; i++) {
+  console.log(buttons[i]);
+  buttons[i].addEventListener("click", tossCoin);
+}
