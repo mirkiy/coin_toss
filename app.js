0faadfc57a5e3f9a614177b8af60fd0bb9dff930
mirkiy <mirizachar@gmail.com>
2023-04-04 19:35:18 +0100

    game logic

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const message = document.querySelector(".message");
 const buttons = document.querySelectorAll("button");
 console.log(buttons);
 
+const coinArray = ["Heads", "Tails"];
+
 function tossCoin(e) {
-    console.log(e.target.innerText)
+  console.log("player:" + e.target.innerText);
+  let computerToss = Math.floor(Math.random() * 2);
+  console.log("computer:" + coinArray[computerToss]);
 }
 
 for (let i = 0; i < buttons.length; i++) {
