da052b9f73d9527a881959a4abf2a442e42a82e9
mirkiy <mirizachar@gmail.com>
2023-04-11 13:38:17 +0100

    end game after certain number of tosses reached

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const buttons = document.querySelectorAll("button");
 
 const coinArray = ["Heads", "Tails"];
 let score = [0, 0];
+let maxCoinToss = 5;
 
 function tossCoin(e) {
   let playerGuess = e.target.innerText;
@@ -17,8 +18,24 @@ function tossCoin(e) {
     output = "Computer Wins ";
     score[1]++;
   }
-  message.innerHTML +=
-    output + "<br> You: " + score[0] + " " + "Computer: " + score[1];
+  if (score[0] + score[1] == 5) {
+    message.innerHTML +=
+      "Game over!" + "<br> You: " + score[0] + " " + "Computer: " + score[1];
+    buttons.forEach((btn) => {
+      btn.disabled = true;
+    });
+    let playAgainBtn = document.createElement("button");
+    playAgainBtn.setAttribute("class", "playAgainBtn");
+    playAgainBtn.textContent = "Play again";
+    let mainDiv = document.querySelector(".mainDiv");
+    mainDiv.appendChild(playAgainBtn);
+    playAgainBtn.addEventListener("click", function () {
+      location.reload();
+    });
+  } else {
+    message.innerHTML +=
+      output + "<br> You: " + score[0] + " " + "Computer: " + score[1];
+  }
 }
 
 for (let i = 0; i < buttons.length; i++) {
